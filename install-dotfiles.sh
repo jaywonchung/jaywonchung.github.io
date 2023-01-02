df3706d95ed06934ab38ff453b85fc44897f8fb0
Jae-Won Chung <jwnchung@umich.edu>
2023-01-02 17:05:47 -0500

    Squash 137 commits

---
+++ install-dotfiles.sh
@@ -0,0 +1,14 @@
+if [ -n "$SSH" ]; then
+  git clone --bare git@github.com:jaywonchung/dotfiles $HOME/.dotfiles
+else
+  git clone --bare https://github.com/jaywonchung/dotfiles.git $HOME/.dotfiles
+fi
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout master
+source .dotmodules/init.sh
+
+if [[ -f "/etc/redhat-release" ]]; then
+  dotfiles checkout rhel-server
+else
+  dotfiles checkout ubuntu-server
+fi
+zsh $HOME/.dotmodules/install/all.sh
